21848eb476d9050463add4494b319938101bb8e9
Fabian Paul <paul.paulfab20@gmail.com>
2022-05-11 13:39:31 +0530

    changes on 11-05-2022 (Pagination )

--- src/response-time-tracking-jira/controllers/jira.request.controller/issue.controller.ts
+++ src/response-time-tracking-jira/controllers/jira.request.controller/issue.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Post } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { JiraIssue } from "src/response-time-tracking-jira/models/issue.entity";
 import { LogDetail } from "src/response-time-tracking-jira/models/logDetail.entity";
 import { IssueService } from "src/response-time-tracking-jira/services/jira-request-services/issue.service";
 
 @Controller('issues')
 export class IssueController {
 
-    constructor(private issueService : IssueService, @InjectRepository(LogDetail) private logRepo){}
+    constructor(private issueService : IssueService, @InjectRepository(LogDetail) private logRepo, @InjectRepository(JiraIssue) private issueRepository){}
 
     @Get('getData')
     async getIssueResponse():Promise<any>{
@@ -22,45 +23,59 @@ export class IssueController {
         console.log('In Controller:::');
 
         let resultData = [];
-        let startVar = 0;
+        //let startVar = 0;
         let result:any = {};
-  
-        do {
-          result =   await this.issueService.post('search',{"fields": ["*all"],"expand": ["changelog"], "maxResults":2, "startAt":startVar});
-          console.log("Result is:::"+result.data);
-          startVar += 2;
+        let arrayOfCustomObject = [];
+
+        //Pagination Call Code
+        for (let startVar=0; ; startVar+=2) {
+          result = await this.issueService.post('search',{"fields": ["*all"],"expand": ["changelog"], "maxResults":2, "startAt":startVar});
           console.log("Next");
-          resultData.push(result);
+          if(result.data.issues.length > 0) {
+            arrayOfCustomObject.push(this.issueService.parseResponseToObject(result));
+            continue;
+          }
+          
+          break;
         }
-        while(result.data.issues.length > 0)
-      
-        return result.data;
+        
+        console.log("Outside do-while");
+        //Inserting data into database after collection from pagination
+        arrayOfCustomObject = arrayOfCustomObject.flat();
+        //console.log(arrayOfCustomObject[0]);
+        //console.log(arrayOfCustomObject[arrayOfCustomObject.length-1]);
+        arrayOfCustomObject.forEach((res) => {
+          console.log(res);
+        })
+        this.issueService.saveToDatabase(arrayOfCustomObject);
+
+        return resultData;
       }
       catch (err) {
+        console.log("Error in Controller");
         console.log(err);
       }
       
     }
-
+    
     @Get('updatedData')
     async getUpdatedResponse():Promise<any> {
     
-    const finalDateVal = "";
-    await this.logRepo.findOne({
-      order : {updated_at : 'DESC'}
-    }).then(async queryResult => {
-        if (queryResult != undefined) {
-          var lastUpdatedDate = queryResult.updated_at;
-          const finalDate = lastUpdatedDate.toISOString().split('T')[0];
-          const result = this.issueService.updateResponse('search',{"fields": ["*all"],"expand": ["changelog"], "jql":"updated > "+`${finalDate}`});
-          return (await result).data;
-        }
-        else {
-          const result = this.issueService.updateResponse('search',{"fields": ["*all"],"expand": ["changelog"], "jql":"updated > 2022-05-04"});
-          return (await result).data;
-        }
-        
-    })
+      const finalDateVal = "";
+      await this.logRepo.findOne({
+        order : {updated_at : 'DESC'}
+      }).then(async queryResult => {
+          if (queryResult != undefined) {
+            var lastUpdatedDate = queryResult.updated_at;
+            const finalDate = lastUpdatedDate.toISOString().split('T')[0];
+            const result = this.issueService.updateResponse('search',{"fields": ["*all"],"expand": ["changelog"], "jql":"updated > "+`${finalDate}`});
+            return (await result).data;
+          }
+          else {
+            const result = this.issueService.updateResponse('search',{"fields": ["*all"],"expand": ["changelog"], "jql":"updated > 2022-05-04"});
+            return (await result).data;
+          }
+      })
     }   
 }
 
--- src/response-time-tracking-jira/models/issue.entity.ts
+++ src/response-time-tracking-jira/models/issue.entity.ts
@@ -3,46 +3,26 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany, UpdateDateColumn, Cr
 @Entity()
 export class JiraIssue {
 
-  //@PrimaryGeneratedColumn()
-  // @Column()
-  // id: string;
-
-  // @Column()
-  // @Generated('increment')
-  // id: number;
-
-  @Column()
+  @Column({
+    default:'',
+  })
   summary: string;
 
-  // @PrimaryColumn({
-  //   name: 'issueKey',
-  //   type: 'varchar',
-  //   length: 32,
-  //   default: ''
-  // })
   @PrimaryColumn()
   issueKey: string;
 
   @Column({
     type: 'json',
     array: false,
-    //default: () => "'[]'",
     nullable: false,
   })
   changelog: Array<{}>;
-
-  // @Column()
-  // status: string;
 
   @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
   public created_at: Date;
 
   @UpdateDateColumn({type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)"})
   public updated_at: Date;
-
-  // function cryptoRandomString(arg0: { length: number; type: string; }): string {
-  //   throw new Error('Function not implemented.');
-  // }
 }
 
 
